464a551403b535c58a444f54ecf3b913106d0691
randomcodenz <neal.blomfield@gmail.com>
2016-10-04 08:30:36 +1300

    Moved responsibility for creating the card into discard playable action
    Moved the responsibility for creating the card from the card name out of the model and into the adapter
    to keep the model as thin as possible

---
+++ app/models/playable_actions/discard_playable_action.rb
@@ -0,0 +1,12 @@
+class DiscardPlayableAction < PlayableAction
+  attr_reader :card_name, :card
+
+  def initialize(card_name:)
+    @card_name = card_name
+    @card = Card.from_s(:card_name => card_name)
+  end
+
+  def apply_to(game_context:)
+    game_context.discard(:card => card)
+  end
+end

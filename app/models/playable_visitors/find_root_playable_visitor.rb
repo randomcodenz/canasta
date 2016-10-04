aafdd28b6b33430059eb4c97813c3c23541e8e52
randomcodenz <neal.blomfield@gmail.com>
2016-10-05 09:55:22 +1300

    Enable an event stream to be replayed
    Added 3 visitors to enable an event stream to be replayed:
    - Find root will traverse up the stream to find and returns the root
    - Apply actions will traverse down the stream apply all actions from each event to the game engine
    - Replay round is a composite of the other 2 and codifies the default usage pattern

---
+++ app/models/playable_visitors/find_root_playable_visitor.rb
@@ -0,0 +1,11 @@
+class FindRootPlayableVisitor
+  def visit(playable)
+    root_playable?(playable) ? playable : visit(playable.parent_playable)
+  end
+
+  private
+
+  def root_playable?(playable)
+    playable.root_playable? || playable.parent_playable.nil?
+  end
+end

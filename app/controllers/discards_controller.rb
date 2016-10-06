ccd03fca23c324175934ffa28cf06d0dde18d28d
randomcodenz <neal.blomfield@gmail.com>
2016-10-07 09:41:17 +1300

    Integrated pick up and discard services into appropriate controllers

--- app/controllers/discards_controller.rb
+++ app/controllers/discards_controller.rb
@@ -1,7 +1,20 @@
 class DiscardsController < ApplicationController
   def create
     round = Round.with_game_and_players.find(params[:round_id])
-    round.player_actions << PlayerActions::Discard.new(:card_name => params[:selected_card])
+
+    discard = Discard.new(:round => round, :card_name => card_name)
+    capture_errors(discard) unless discard.call
+
     redirect_to round
+  end
+
+  private
+
+  def card_name
+    params[:selected_card]
+  end
+
+  def capture_errors(discard)
+    flash[:errors] = discard.errors
   end
 end
--- app/controllers/pick_up_cards_controller.rb
+++ app/controllers/pick_up_cards_controller.rb
@@ -1,7 +1,16 @@
 class PickUpCardsController < ApplicationController
   def create
     round = Round.with_game_and_players.find(params[:round_id])
-    round.player_actions << PlayerActions::PickUpCards.new
+
+    pick_up_cards = PickUpCards.new(:round => round)
+    capture_errors(pick_up_cards) unless pick_up_cards.call
+
     redirect_to round
+  end
+
+  private
+
+  def capture_errors(pick_up_cards)
+    flash[:errors] = pick_up_cards.errors
   end
 end
